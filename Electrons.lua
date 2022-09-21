9531661c93c4c4d50737cbefc6dac89d6022d0b7
R1zve <99558260+R1zve@users.noreply.github.com>
2022-09-21 13:08:00 -0400

    Create Electrons.lua

---
+++ Electrons.lua
@@ -0,0 +1,16 @@
+local Modules = {
+	Icons = {
+		["Home"] = 0,
+		["Farming"] = 0,
+		["Mobs"] = 0,
+		["Machinery"] = 0,
+		["Block Printer"] = 0,
+		["Teleports"] = 0,
+		["Miscellaneous"] = 0,
+		["Themes"] = 0,
+		["Settings"] = 0
+	},
+	
+}
+
+return Modules

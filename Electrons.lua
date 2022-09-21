f981fef1d8370101bc352046d5808af13fdc61bf
R1zve <99558260+R1zve@users.noreply.github.com>
2022-09-21 14:03:52 -0400

    Update Electrons.lua

--- Electrons.lua
+++ Electrons.lua
@@ -1,14 +1,14 @@
 local Modules = {
 	Icons = {
-		["Home"] = "10973655328",
-		["Farming"] = "10973688593",
-		["Mobs"] = "10973744847",
-		["Machinery"] = "10973715601",
-		["Block Printer"] = "10973641262",
-		["Teleports"] = "10973738094",
-		["Miscellaneous"] = "10973660130",
-		["Themes"] = "10973699863",
-		["Settings"] = "10973695138"
+		["Home"] = 10973655321,
+		["Farming"] = 10973688580,
+		["Mobs"] = 10973744839,
+		["Machinery"] = 10973715557,
+		["Block Printer"] = 10973641254,
+		["Teleports"] = 10973738071,
+		["Miscellaneous"] = 10973660119,
+		["Themes"] = 10973699847,
+		["Settings"] = 10973695126
 	},
 }
 
